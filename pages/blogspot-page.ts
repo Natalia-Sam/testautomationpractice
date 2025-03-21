967fd5b2735e013116eca19e9d5547602c30aaec
Natalia-Samarskaya <samarskaaya@gmail.com>
2025-03-21 20:28:34 +0200

    added tests

--- pages/blogspot-page.ts
+++ pages/blogspot-page.ts
@@ -6,8 +6,53 @@ export default class BlogspotPage {
   readonly emailInput: Locator;
   readonly phoneInput: Locator;
   readonly addressInput: Locator;
+
+  // Gender Locators
   readonly maleSelector: Locator;
   readonly femaleSelector: Locator;
+
+  // Days of week locators
+  readonly sundaySelector: Locator;
+  readonly mondaySelector: Locator;
+  readonly tuesdaySelector: Locator;
+  readonly wednesdaySelector: Locator;
+  readonly thursdaySelector: Locator;
+  readonly fridaySelector: Locator;
+  readonly saturdaySelector: Locator;
+
+  // Country locators
+  readonly countryDropdown: Locator;
+  readonly usaOption: Locator;
+  readonly canadaOption: Locator;
+  readonly ukOption: Locator;
+  readonly germanyOption: Locator;
+  readonly franceOption: Locator;
+  readonly australiaOption: Locator;
+  readonly japanOption: Locator;
+  readonly chinaOption: Locator;
+  readonly brazilOption: Locator;
+  readonly indiaOption: Locator;
+
+  // Color locators
+  readonly colorsList: Locator;
+  readonly redColor: Locator;
+  readonly blueColor: Locator;
+  readonly greenColor: Locator;
+  readonly yellowColor: Locator;
+  readonly whiteColor: Locator;
+
+  // Animal Locators
+  readonly animalsSortedList: Locator;
+  readonly catAnimal: Locator;
+  readonly cheetahAnimal: Locator;
+  readonly deerAnimal: Locator;
+  readonly dogAnimal: Locator;
+  readonly elephantAnimal: Locator;
+  readonly foxAnimal: Locator;
+  readonly giraffeAnimal: Locator;
+  readonly lionAnimal: Locator;
+  readonly rabbitAnimal: Locator;
+  readonly zebraAnimal: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -15,8 +60,55 @@ export default class BlogspotPage {
     this.emailInput = this.page.locator("//input[@id='email']");
     this.phoneInput = this.page.locator("//input[@id='phone']");
     this.addressInput = this.page.locator("//textarea[@class='form-control']");
+
+    // Gender Locators
     this.maleSelector = this.page.locator("//input[@id='male']");
     this.femaleSelector = this.page.locator("//input[@id='female']");
+
+    // Days of week locators
+    // this.sundayCheck = this.page.locator("//input[@id='sunday']");
+    // this.sundayCheck = this.page.getByRole('input', { id: 'sunday' });
+    this.sundaySelector = this.page.locator("#sunday");
+    this.mondaySelector = this.page.locator("#monday");
+    this.tuesdaySelector = this.page.locator("#tuesday");
+    this.wednesdaySelector = this.page.locator("#wednesday");
+    this.thursdaySelector = this.page.locator("#thursday");
+    this.fridaySelector = this.page.locator("#friday");
+    this.saturdaySelector = this.page.locator("#saturday");
+
+    // Country locators
+    this.countryDropdown = this.page.locator("#country");
+    this.usaOption = this.page.locator("//option[@value='usa']");
+    this.canadaOption = this.page.locator("//option[@value='canada']");
+    this.ukOption = this.page.locator("//option[@value='uk']");
+    this.germanyOption = this.page.locator("//option[@value='germany']");
+    this.franceOption = this.page.locator("//option[@value='france']");
+    this.australiaOption = this.page.locator("//option[@value='australia']");
+    this.japanOption = this.page.locator("//option[@value='japan']");
+    this.chinaOption = this.page.locator("//option[@value='china']");
+    this.brazilOption = this.page.locator("//option[@value='brazil']");
+    this.indiaOption = this.page.locator("//option[@value='india']");
+
+    // Color locators
+    this.colorsList = this.page.locator("//select[@id='colors']");
+    this.redColor = this.page.locator("//option[@value='red']");
+    this.blueColor = this.page.locator("//option[@value='blue']");
+    this.greenColor = this.page.locator("//option[@value='green']");
+    this.yellowColor = this.page.locator("//option[@value='yellow']");
+    this.whiteColor = this.page.locator("//option[@value='white']");
+
+    // Animal Locators
+    this.animalsSortedList = this.page.locator("#animals");
+    this.catAnimal = this.page.locator("//option[@value='cat']");
+    this.cheetahAnimal = this.page.locator("//option[@value='cheetah']");
+    this.deerAnimal = this.page.locator("//option[@value='deer']");
+    this.dogAnimal = this.page.locator("//option[@value='dog']");
+    this.elephantAnimal = this.page.locator("//option[@value='elephant']");
+    this.foxAnimal = this.page.locator("//option[@value='fox']");
+    this.giraffeAnimal = this.page.locator("//option[@value='giraffe']");
+    this.lionAnimal = this.page.locator("//option[@value='lion']");
+    this.rabbitAnimal = this.page.locator("//option[@value='rabbit']");
+    this.zebraAnimal = this.page.locator("//option[@value='zebra']");
   }
 
   public async enterName(name: string) {
@@ -43,5 +135,69 @@ export default class BlogspotPage {
 
   public async selectFemaleGender() {
     await this.femaleSelector.click();
+  }
+
+  public async selectSunday() {
+    await this.sundaySelector.click();
+  }
+
+  public async selectMonday() {
+    await this.mondaySelector.click();
+  }
+
+  public async selectTuesday() {
+    await this.tuesdaySelector.click();
+  }
+
+  public async selectWednesday() {
+    await this.wednesdaySelector.click();
+  }
+
+  public async selectThursday() {
+    await this.thursdaySelector.click();
+  }
+
+  public async selectFriday() {
+    await this.fridaySelector.click();
+  }
+
+  public async selectSaturday() {
+    await this.saturdaySelector.click();
+  }
+
+  public async openCountryDropdown() {
+    await this.countryDropdown.click();
+  }
+
+  public async selectBrazilCountryDropdown() {
+    await this.brazilOption.click();
+  }
+
+  public async selectCanadaCountryDropdown() {
+    await this.canadaOption.click();
+  }
+
+  public async colorListFocus() {
+    await this.colorsList.click();
+  }
+
+  public async selectBlue() {
+    await this.blueColor.click();
+  }
+
+  public async selectGreen() {
+    await this.greenColor.click();
+  }
+
+  public async animalsListFocus() {
+    await this.animalsSortedList.click();
+  }
+
+  public async selectFox() {
+    await this.foxAnimal.click();
+  }
+
+  public async selectGiraffe() {
+    await this.giraffeAnimal.click();
   }
 }
--- tests/mainpage.spec.ts
+++ tests/mainpage.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from "@playwright/test";
 import { test } from "../fixtures";
 import { assert } from "console";
+import { TIMEOUT } from "dns";
 
 const name = "Natalia";
 const email = "mytest.111888@gmail.com";
@@ -17,16 +18,20 @@ test.describe("Open main page", () => {
     await expect(blogspotPage.nameInput).toBeEmpty();
     await blogspotPage.enterName(name);
     await expect(blogspotPage.nameInput).toHaveValue(name);
+    //  🔴 Тут тест падає, бо повертає пусту строку
     // await expect(blogspotPage.nameInput).toHaveText('Natalia');
     await expect(name.length).toBeLessThanOrEqual(10);
     await expect(name).toMatch(/^[A-Z][A-Za-z\s'-]*$/);
-    await expect(blogspotPage.nameInput).toHaveValue(/^[A-Z][A-Za-z\s'-]{1,10}$/);
+    await expect(blogspotPage.nameInput).toHaveValue(
+      /^[A-Z][A-Za-z\s'-]{1,10}$/,
+    );
   });
 
   test("Validate entered email", async ({ page, blogspotPage }) => {
     await expect(blogspotPage.emailInput).toBeEmpty();
     await blogspotPage.enterEmail(email);
     await expect(blogspotPage.emailInput).toHaveValue(email);
+    //  🔴 Тут тест падає, бо повертає пусту строку
     // await expect(blogspotPage.emailInput).toHaveText('mytest.111888@gmail.com');
     // await expect(blogspotPage.emailInput).toMatch(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
   });
@@ -57,5 +62,72 @@ test.describe("Open main page", () => {
     await expect(blogspotPage.femaleSelector).toBeChecked;
     await expect(blogspotPage.maleSelector).not.toBeChecked;
   });
-  
+
+  test("Select Sunday", async ({ page, blogspotPage }) => {
+    await expect(blogspotPage.sundaySelector).not.toBeChecked();
+    await blogspotPage.selectSunday();
+    await expect(blogspotPage.sundaySelector).toBeChecked;
+  });
+
+  test("Select all weekdays", async ({ page, blogspotPage }) => {
+    await expect(blogspotPage.sundaySelector).not.toBeChecked();
+    await expect(blogspotPage.mondaySelector).not.toBeChecked();
+    await expect(blogspotPage.tuesdaySelector).not.toBeChecked();
+    await expect(blogspotPage.wednesdaySelector).not.toBeChecked();
+    await expect(blogspotPage.thursdaySelector).not.toBeChecked();
+    await expect(blogspotPage.fridaySelector).not.toBeChecked();
+    await expect(blogspotPage.saturdaySelector).not.toBeChecked();
+    await blogspotPage.selectMonday();
+    await blogspotPage.selectTuesday();
+    await blogspotPage.selectWednesday();
+    await blogspotPage.selectThursday();
+    await blogspotPage.selectFriday();
+    await expect(blogspotPage.sundaySelector).not.toBeChecked();
+    await expect(blogspotPage.mondaySelector).toBeChecked();
+    await expect(blogspotPage.tuesdaySelector).toBeChecked();
+    await expect(blogspotPage.wednesdaySelector).toBeChecked();
+    await expect(blogspotPage.thursdaySelector).toBeChecked();
+    await expect(blogspotPage.fridaySelector).toBeChecked();
+    await expect(blogspotPage.saturdaySelector).not.toBeChecked();
+  });
+
+  test("Select Brazil in country dropdown list", async ({
+    page,
+    blogspotPage,
+  }) => {
+    // 🔴 Тут тест падає
+    // await expect(blogspotPage.countryDropdown).toHaveText('United States');
+    await blogspotPage.openCountryDropdown();
+    await blogspotPage.selectBrazilCountryDropdown();
+    await expect(blogspotPage.countryDropdown).toHaveText("Brazil");
+  });
+
+  // test("Select county in dropdown list", async ({ blogspotPage }) => {
+  //   await blogspotPage.openCountryDropdown();
+  //   await new Promise(resolve => setTimeout(resolve, 10000));
+  // }, 15000);
+
+  // 🔴 Тут не обираються опшини з дропдауна і тест падає
+  // test("Switch between countries in dropdown list", async ({ page, blogspotPage }) => {
+  //   // await expect(blogspotPage.countryDropdown).toHaveText('United States');
+  //   await blogspotPage.openCountryDropdown();
+  //   await blogspotPage.selectBrazilCountryDropdown();
+  //   await expect(blogspotPage.countryDropdown).toHaveText('Brazil');
+  //   await blogspotPage.selectCanadaCountryDropdown();
+  //   await expect(blogspotPage.countryDropdown).toHaveText('Canada');
+  // });
+
+  test("Select a blue color", async ({ page, blogspotPage }) => {
+    await blogspotPage.colorListFocus();
+    await blogspotPage.selectBlue();
+    await expect(blogspotPage.blueColor).toHaveJSProperty("disabled", false);
+    //  🔴 Як перевірити, що інший елемент не обран?
+    // await expect(blogspotPage.greenColor).toHaveJSProperty("disabled", true);
+  });
+
+  test.only("Select a Giraffe animal", async ({ page, blogspotPage }) => {
+    await blogspotPage.animalsListFocus();
+    await blogspotPage.selectGiraffe();
+    await expect(blogspotPage.giraffeAnimal).toHaveJSProperty("disabled", false);
+  });
 });
